ef6f5cd687b9a404a804c666d78a5aa3e6be1f20
Olivia Adler <theoliviaadler@gmail.com>
2022-06-07 02:03:36 +0430

    add: test fo swiper.

---
+++ src/pages/Intro-slides/indix.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Pagination } from 'swiper';
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        pagination={{
+          type: 'progressbar',
+        }}
+        navigation
+        modules={[Pagination]}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
