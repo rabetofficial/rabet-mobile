3b7394784cb9547401a201799a620fca8f4ce22e
Olivia Adler <theoliviaadler@gmail.com>
2022-06-07 03:21:17 +0430

    fix: swiper for Intro.

--- src/pages/Intro-slides/Slide2.tsx
+++ src/pages/Intro-slides/Slide2.tsx
@@ -10,7 +10,7 @@ const SecondSlide = () => (
       <SlideTwo />
     </S.ImgSlideTwo>
     <S.TextContainer>
-      <S.HeadText className="mt-[51px]">Secure by default</S.HeadText>
+      <S.HeadText>Secure by default</S.HeadText>
       <S.MainText>
         All the data in the Rabet is encrypted and stored on your
         local device. Therefore, you are in complete control of your
---
+++ src/pages/Intro-slides/indwx.tsx
@@ -0,0 +1,89 @@
+import { Slide } from 'react-slideshow-image';
+import React, { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+
+import { SlideLeft, SlideRight } from 'svgs/longArrowCircle';
+import SlidesLayout from 'components/common/Layouts/SlidesLayout';
+
+import config from 'config';
+import mypic from 'public/images/stellar-black.png';
+import Slide1 from './Slide1';
+import Slide2 from './Slide2';
+import Slide3 from './Slide3';
+import Slide4 from './Slide4';
+
+import * as S from './styles';
+
+const Slides = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const slideRef = useRef<Slide>(null);
+
+  const handleKey = ({ key }: { key: string }) => {
+    if (key === 'ArrowLeft') {
+      slideRef?.current?.goBack();
+    }
+
+    if (key === 'ArrowRight') {
+      slideRef?.current?.goNext();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKey);
+
+    return () => {
+      document.removeEventListener('keydown', handleKey);
+    };
+  }, []);
+
+  const properties = {
+    autoplay: false,
+    canSwipe: false,
+    transitionDuration: config.SLIDESHOW_TRANSITION,
+    infinite: false,
+    arrows: true,
+    prevArrow: (
+      <S.LeftCircle
+        thirdSlide={slideIndex === 3}
+        disabled={slideIndex === 0}
+      >
+        <SlideLeft disabled={slideIndex === 0} />
+      </S.LeftCircle>
+    ),
+    nextArrow: (
+      <S.RightCircle
+        thirdSlide={slideIndex === 3}
+        disabled={slideIndex === 0}
+      >
+        <SlideRight disabled={slideIndex === 3} />
+      </S.RightCircle>
+    ),
+    onChange: (_: number, newIndex: number) => {
+      setSlideIndex(newIndex);
+    },
+    indicators: (i: number) =>
+      slideIndex === 3 ? (
+        <div />
+      ) : (
+        <S.Indicators index={i} disabled={i > slideIndex} />
+      ),
+  };
+
+  return (
+    <div onKeyDown={handleKey}>
+      <Image src={mypic} width={42} height={36} />
+      <SlidesLayout>
+        <S.SlidesContainer>
+          <Slide {...properties} ref={slideRef}>
+            <Slide1 />
+            <Slide2 />
+            <Slide3 />
+            <Slide4 />
+          </Slide>
+        </S.SlidesContainer>
+      </SlidesLayout>
+    </div>
+  );
+};
+
+export default Slides;
