51541c9be2e1f4c964ae41156175ffb407fb0749
Marinaa-dev <meri.rhb@gmail.com>
2022-06-07 20:11:18 +0430

    feat: add confirm login page

---
+++ src/pages/confirm-login/index.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+
+import Logo from 'components/Logo';
+import Layout from 'components/common/Layouts/BaseLayout';
+import Input from 'components/common/Input';
+import Error from 'components/common/Error';
+import Button from 'components/common/Button';
+import ButtonContainer from 'components/common/ButtonContainer';
+
+type FormValues = {
+  password: string;
+  confirm: string;
+};
+
+const ConfirmLogin = () => {
+  const onSubmit = (values: FormValues) => {};
+
+  const validateForm = (values: FormValues) => {};
+
+  return (
+    <Layout>
+      <Logo />
+
+      <Form
+        onSubmit={onSubmit}
+        validate={validateForm}
+        render={({ submitError, handleSubmit, invalid }) => (
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <Field name="password">
+              {({ input, meta }) => (
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  size="medium"
+                  variant="password"
+                  className="!mt-[40px]"
+                  input={input}
+                  meta={meta}
+                  autoFocus
+                />
+              )}
+            </Field>
+
+            <Field name="confirm">
+              {({ input, meta }) => (
+                <Input
+                  type="password"
+                  placeholder="Confirm Password"
+                  size="medium"
+                  variant="password"
+                  input={input}
+                  meta={meta}
+                  className="!mt-6"
+                />
+              )}
+            </Field>
+
+            {submitError && <Error>{submitError}</Error>}
+
+            <ButtonContainer fixedBottom>
+              <Button
+                type="submit"
+                variant="primary"
+                size="medium"
+                content="Continue"
+                className="mb-8"
+                disabled={invalid}
+              />
+            </ButtonContainer>
+          </form>
+        )}
+      />
+    </Layout>
+  );
+};
+
+export default ConfirmLogin;
