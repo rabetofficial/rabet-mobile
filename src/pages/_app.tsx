d66c77287847a74b5e650fe18fa9162b0453684d
Marinaa-dev <meri.rhb@gmail.com>
2022-06-26 13:15:35 +0430

    fix: fix full page transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,12 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { useRouter } from 'next/router';
-import { useTransition, animated } from '@react-spring/web';
+import { animated, Transition } from 'react-spring';
 
 import 'react-slideshow-image/dist/styles.css';
 import 'tippy.js/dist/svg-arrow.css';
 import 'tippy.js/animations/scale.css';
 import 'tippy.js/animations/shift-away.css';
-import 'react-spring-bottom-sheet/dist/style.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'styles/global.css';
@@ -28,39 +27,46 @@ function MyApp({ Component, pageProps }: AppProps) {
     },
   ];
 
-  const transitions = useTransition(items, {
-    keys: (item) => item.id,
-    from: { opacity: 0 },
-    initial: { opacity: 0 },
-    enter: { opacity: 1, position: 'relative' },
-    leave: { opacity: 0, position: 'absolute' },
-    config: { duration: 150 },
-  });
-
   return (
     <ThemeProvider theme={theme}>
       <Global theme={theme} />
       <Provider store={store}>
-        {transitions(
-          (
-            props,
-            {
-              pageProps: animatedPageProps,
-              Component: AnimatedComponent,
-            },
-          ) => (
-            <animated.div
-              style={{
-                // ...props,
-                width: '100%',
-                height: '100%',
-                overflow: 'hidden',
-              }}
-            >
-              <AnimatedComponent {...animatedPageProps} />
-            </animated.div>
-          ),
-        )}
+        <div className="page-transition-wrapper">
+          <Transition
+            items={items}
+            keys={(item: any) => item.id}
+            from={{
+              opacity: 0,
+              transform: 'translateX(100vw)',
+            }}
+            enter={{
+              opacity: 1,
+              transform: 'translateX(0)',
+            }}
+            leave={{
+              opacity: 0,
+              transform: 'translateX(-20vw)',
+              position: 'absolute',
+              inset: '0',
+            }}
+            onRest={() => window.scrollTo(0, 0)}
+          >
+            {(
+              styles,
+              {
+                pageProps: animatedPageProps,
+                Component: AnimatedComponent,
+              },
+            ) => (
+              <animated.div
+                className="page-transition-container"
+                style={{ ...styles }}
+              >
+                <AnimatedComponent {...animatedPageProps} />
+              </animated.div>
+            )}
+          </Transition>
+        </div>
       </Provider>
     </ThemeProvider>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,41 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  html, body, #__next {
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+  }
+
+  #__next {
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .page-transition-wrapper {
     height: 100%;
   }
 
-  body.dashboard {
-    background: ${({ theme }) => theme.colors.primary.lighter};
-    padding-right: 54px;
-    padding-left: 54px;
+  .page-transition-container {
+    overflow: hidden;
+    max-width: 100%;
+    width: 100%;
+    height: 100%;
+  }
+
+  main {
+    align-items: stretch;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    overflow-scrolling: touch;
+    -webkit-overflow-scrolling: touch;
+    margin: 0;
   }
 
   .layout {
