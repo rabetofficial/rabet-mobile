ee4eba209985fe55b3b83dc7440651620d396d23
Marinaa-dev <meri.rhb@gmail.com>
2022-07-05 12:48:37 +0430

    feat: add private key

--- src/blocks/PrivateKey/index.tsx
+++ src/blocks/PrivateKey/index.tsx
@@ -1,64 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Field } from 'react-final-form';
+import styled from 'styled-components';
 
 import Input from 'components/common/Input';
 import Error from 'components/common/Error';
 import Button from 'components/common/Button';
 import Layout from 'components/common/Layouts/BaseLayout';
 import ButtonContainer from 'components/common/ButtonContainer';
+import Card from 'components/common/Card';
+import currentActiveAccount from 'utils/activeAccount';
+import CopyKey from 'components/common/CopyKey';
+
+const Msg = styled.div`
+  font-size: 14px;
+  line-height: 1.43;
+  color: ${({ theme }) => theme.colors.primary.dark};
+  margin-top: 8px;
+
+  span {
+    color: black;
+    font-weight: 500;
+  }
+`;
 
 export type FormValues = {
   key: string;
 };
 
 const PrivateKey = () => {
-  const onSubmit = () => {};
+  const { activeAccount } = currentActiveAccount();
+  const [show, setShow] = useState(false);
+  const onSubmit = (value: FormValues) => {
+    if (value.key === '123456') {
+      setShow(true);
+    }
+  };
   return (
     <Layout className="mt-6">
-      <Form
-        onSubmit={onSubmit}
-        render={({
-          pristine,
-          submitting,
-          submitError,
-          handleSubmit,
-        }) => (
-          <form
-            className="form"
-            onSubmit={handleSubmit}
-            autoComplete="off"
-          >
-            <Field name="key">
-              {({ input, meta }) => (
-                <>
-                  <label className="label-primary">Password</label>
-                  <Input
-                    type="password"
+      <>
+        {show ? (
+          <>
+            <Msg>
+              <span>Do not lose it!</span> It cannot be recovered if
+              you lose it. <br />
+              <span>Do not share it!</span> Your funds will be stolen
+              if you use this file on a phishing site.
+            </Msg>
+            <div className="label-primary mb-[6px] mt-[32px] font-medium">
+              Private Key
+            </div>
+
+            <Card type="primary" className="shadow-transparent">
+              <CopyKey keyValue={activeAccount.privateKey} />
+            </Card>
+          </>
+        ) : (
+          <Form
+            onSubmit={onSubmit}
+            render={({
+              pristine,
+              submitting,
+              submitError,
+              handleSubmit,
+            }) => (
+              <form
+                className="form"
+                onSubmit={handleSubmit}
+                autoComplete="off"
+              >
+                <Field name="key">
+                  {({ input, meta }) => (
+                    <>
+                      <label className="label-primary">
+                        Password
+                      </label>
+                      <Input
+                        type="password"
+                        size="medium"
+                        variant="password"
+                        placeholder="Enter your password"
+                        input={input}
+                        meta={meta}
+                        autoFocus
+                      />
+                    </>
+                  )}
+                </Field>
+
+                {submitError && <Error>{submitError}</Error>}
+
+                <ButtonContainer fixedBottom mb={32}>
+                  <Button
+                    type="submit"
+                    variant="primary"
                     size="medium"
-                    variant="password"
-                    placeholder="Enter your password"
-                    input={input}
-                    meta={meta}
-                    autoFocus
+                    content="Show"
+                    disabled={pristine || submitting}
                   />
-                </>
-              )}
-            </Field>
-
-            {submitError && <Error>{submitError}</Error>}
-
-            <ButtonContainer fixedBottom mb={32}>
-              <Button
-                type="submit"
-                variant="primary"
-                size="medium"
-                content="Show"
-                disabled={pristine || submitting}
-              />
-            </ButtonContainer>
-          </form>
+                </ButtonContainer>
+              </form>
+            )}
+          />
         )}
-      />
+      </>
     </Layout>
   );
 };
