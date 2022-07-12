99d9fb2074afafa3603ffa0b86b32a900742707c
Alper Polat <alper95polat@gmail.com>
2022-07-12 10:45:24 +0430

    fix: use indexedDB for storage

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,34 +1,98 @@
 import { encrypt, decrypt } from './crypto';
 
+const onUpgradeNeeded = (e) => {
+  const db = e.target.result;
+
+  db.createObjectStore('data', { keyPath: 'id' });
+
+  db.createObjectStore('store', {
+    keyPath: 'id',
+  });
+
+  db.createObjectStore('connectedWebsites', {
+    keyPath: 'id',
+  });
+};
+
+const setInDb = (key: string, value: any) => {
+  const request = indexedDB.open('rabet', 1);
+
+  request.onupgradeneeded = onUpgradeNeeded;
+
+  request.onerror = (e) => {
+    throw e;
+  };
+
+  request.onsuccess = (e) => {
+    const db = e.target.result;
+
+    const tx = db.transaction(key, 'readwrite');
+
+    if (Array.isArray(value)) {
+      for (let i = 0; i < value.length; i += 1) {
+        tx.objectStore(key).add({
+          id: Math.random(),
+          value: value[i],
+        });
+      }
+    } else {
+      tx.objectStore(key).clear();
+      tx.objectStore(key).add({ id: 'only', value });
+    }
+  };
+};
+
 export const get = (key: string, password?: string) =>
   new Promise((resolve, reject) => {
-    const result = localStorage.getItem(key);
-    const data = result[key];
+    const request = indexedDB.open('rabet');
 
-    if (!data) {
-      return resolve(null);
-    }
+    request.onupgradeneeded = onUpgradeNeeded;
 
-    if (!password) {
-      return resolve(data);
-    }
+    request.onsuccess = (e) => {
+      const db = e.target.result;
 
-    const decrypredData = decrypt(password, data);
-    let jsonData;
+      const tx = db.transaction(key, 'readonly');
+      const table = tx.objectStore(key);
+      const req = table.openCursor();
 
-    try {
-      jsonData = JSON.parse(decrypredData);
-    } catch (e) {
-      return reject();
-    }
+      req.onsuccess = (e) => {
+        const cursor = e.target.result;
 
-    return resolve(jsonData);
+        if (cursor) {
+          if (cursor.key === 'only') {
+            if (!password) {
+              resolve(cursor.value.value);
+            } else {
+              const decrypredData = decrypt(
+                password,
+                cursor.value.value,
+              );
+
+              let jsonData;
+
+              try {
+                jsonData = JSON.parse(decrypredData);
+              } catch (e) {
+                return reject();
+              }
+
+              return resolve(jsonData);
+            }
+            console.log('The only data: ', cursor.value.value);
+          } else {
+            console.log(`Not the only value ${cursor.value}`);
+          }
+        } else {
+          resolve(null);
+        }
+      };
+    };
   });
 
 export const set = (key: string, value: any, password?: string) =>
   new Promise((resolve, reject) => {
     try {
-      let dataToBeSet;
+      let dataToBeSet = value;
 
       if (password) {
         const encryptedData = encrypt(
@@ -37,12 +101,11 @@ export const set = (key: string, value: any, password?: string) =>
         );
 
         dataToBeSet = encryptedData;
-      } else {
-        dataToBeSet = JSON.stringify(value);
       }
 
-      localStorage.setItem(key, dataToBeSet);
+      setInDb(key, dataToBeSet);
     } catch (e) {
-      reject();
+      console.log(e);
+      reject(e);
     }
   });
