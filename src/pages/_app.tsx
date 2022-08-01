6865fa97afd7265ad7aea1f359654610eac6a9f6
Marinaa-dev <meri.rhb@gmail.com>
2022-08-01 11:18:34 +0430

    fix: change full page transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { useRouter, NextRouter } from 'next/router';
-import { animated, Transition } from 'react-spring';
+import { useRouter } from 'next/router';
 
 import { Page } from 'models';
 import theme from 'styles/theme';
@@ -12,6 +11,7 @@ import pages from 'staticRes/exitPages';
 import ExtTitle from 'components/common/ExitTitle';
 import RoutesManager from 'components/RoutesManager';
 import Opening from 'components/Opening';
+import SpringLoad from 'components/common/SpringLoad';
 
 import 'react-slideshow-image/dist/styles.css';
 import 'tippy.js/dist/svg-arrow.css';
@@ -27,21 +27,6 @@ import store from '../store';
 function MyApp({ Component, pageProps }: AppProps) {
   const [opening, setOpening] = useState(true);
   const router = useRouter();
-  const items = [
-    {
-      id: router.route,
-      Component,
-      pageProps,
-    },
-  ];
-
-  const finalDepth = 3;
-
-  const getPathDepth = (location: NextRouter) => {
-    let pathArr = location.pathname.split('/');
-    pathArr = pathArr.filter((n) => n !== '');
-    return pathArr.length;
-  };
 
   const [page, setPage] = useState<Page | undefined>(undefined);
 
@@ -70,53 +55,17 @@ function MyApp({ Component, pageProps }: AppProps) {
             {router.pathname === '/home' ? (
               <Component {...pageProps} />
             ) : (
-              <Transition
-                items={items}
-                keys={(item: any) => item.id}
-                from={{
-                  opacity: 0,
-                  transform:
-                    page && getPathDepth(router) < finalDepth
-                      ? 'translateX(-100vw)'
-                      : 'translateX(100vw)',
-                }}
-                enter={{
-                  opacity: 1,
-                  transform: 'translateX(0)',
-                }}
-                leave={{
-                  opacity: 0,
-                  transform:
-                    page && getPathDepth(router) < finalDepth
-                      ? 'translateX(20vw)'
-                      : 'translateX(-20vw)',
-                  position: 'absolute',
-                  inset: '0',
-                }}
-              >
-                {(
-                  styles,
-                  {
-                    pageProps: animatedPageProps,
-                    Component: AnimatedComponent,
-                  },
-                ) => (
-                  <animated.div
-                    className="page-transition-container"
-                    style={{ ...styles }}
-                  >
-                    <>
-                      {page && (
-                        <ExtTitle
-                          title={page.title}
-                          borderless={page.borderless}
-                        />
-                      )}
-                      <AnimatedComponent {...animatedPageProps} />
-                    </>
-                  </animated.div>
+              <>
+                {page && (
+                  <ExtTitle
+                    title={page.title}
+                    borderless={page.borderless}
+                  />
                 )}
-              </Transition>
+                <SpringLoad>
+                  <Component {...pageProps} />
+                </SpringLoad>
+              </>
             )}
           </div>
         </RoutesManager>
