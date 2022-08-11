0b30873c84ff94c6634798aa3db3a6fc7c7771c4
Oliva Adler <theoliviaadler@gmail.com>
2022-08-11 21:54:31 +0430

    try: adding color to notch.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
-          content="default"
+          content="black-translucent"
         />
         <meta name="apple-mobile-web-app-title" content="Rabet" />
         <meta
@@ -51,7 +51,7 @@ export default function Document() {
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
 
         <link
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,10 @@ const GlobalStyle = createGlobalStyle`
     overflow-x: hidden;
   }
   
+  body{
+    padding: env(safe-area-inset-top, 20px) env(safe-area-inset-right, 20px) env(safe-area-inset-bottom, 20px) env(safe-area-inset-left, 20px);
+  }
+
   * {
     -webkit-tap-highlight-color:transparent;
   }
