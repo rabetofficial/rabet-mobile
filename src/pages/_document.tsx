cf0105343222a0bb385d5bb828e3e57d6281e99c
Oliva Adler <theoliviaadler@gmail.com>
2022-08-11 22:47:53 +0430

    remove: test.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
+          content="default"
         />
         <meta name="apple-mobile-web-app-title" content="Rabet" />
         <meta
@@ -51,7 +51,7 @@ export default function Document() {
 
         <meta
           name="viewport"
-          content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
 
         <link
