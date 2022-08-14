cba88e67733e7b29b480618bdbed786e17061871
Alper Polat <alper95polat@gmail.com>
2022-08-14 19:53:46 +0430

    fix: detect mobile in choose-browser

--- src/blocks/chooseBrowser/index.tsx
+++ src/blocks/chooseBrowser/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import detectMobile from 'utils/detectMobile';
 
 import detectOS from 'utils/detectOS';
 
 const ChooseBrowser = () => {
   const os = detectOS();
+  const isMobile = detectMobile();
 
   const text = os === 'ios' ? 'Safari' : 'Chrome';
 
   return (
     <div className="h-screen flex justify-center items-center">
-      <p className="text-lg">Use {text} to install the app</p>
+      {isMobile ? (
+        <p className="text-lg">Use {text} to install the app</p>
+      ) : (
+        <p>Use phone to install the app</p>
+      )}
     </div>
   );
 };
